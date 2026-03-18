bd66e50a2a80f9f0a4cca81a22df57e82b1d9c06
kndxhz <kndxhz@163.com>
2026-03-18 23:54:59 +0800

    fix: main入口函数

--- main.go
+++ main.go
@@ -8,32 +8,37 @@ import (
 
 var base_163 = "https://music.163.com"
 
+func main() {
+	var song_id string = "144441"
+	get_lyrics(song_id)
+}
+
 func get_lyrics(song_id string) {
+	//获取网易云歌词接口
+	url := base_163 + "/api/song/lyric?id=" + song_id + "&os=pc&lv=-1"
+	method := "GET"
 
-   url := base_163 + "/api/song/lyric?id=" + song_id + "&os=pc&lv=-1"
-   method := "GET"
+	client := &http.Client {
+	}
+	req, err := http.NewRequest(method, url, nil)
 
-   client := &http.Client {
-   }
-   req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0")
 
-   if err != nil {
-      fmt.Println(err)
-      return
-   }
-   req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (HTML, like Gecko) Chrome/126.0.0.0 Safari/537.36 Edg/126.0.0.0")
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
 
-   res, err := client.Do(req)
-   if err != nil {
-      fmt.Println(err)
-      return
-   }
-   defer res.Body.Close()
-
-   body, err := io.ReadAll(res.Body)
-   if err != nil {
-      fmt.Println(err)
-      return
-   }
-   fmt.Println(string(body))
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(body))
 }
